a5ccafac7c502ed08bf2e861704bc82185ea2ffe
Gabriel Florit <gabriel.florit@velir.com>
2011-12-16 12:12:41 -0800

    drawing many blairs with canvas

--- gabrielflor_it/static/js/blair.js
+++ gabrielflor_it/static/js/blair.js
@@ -1,110 +1,36 @@
 (function() {
 
-var masterBlairs;
-var numberOfBlairs;
-var totalNumberOfBlairs = 8;
-
 $(document).ready(function() {
 
-	var files = [
-					'blair00', 
-					'blair10', 
-					'blair20', 
-					'blair30', 
-					'blair40', 
-					'blair50'];
-	
-	masterBlairs = [];
-	numberOfBlairs = files.length;
+	var canvas = document.getElementById("blair");
+	var ctx = canvas.getContext("2d");
 
+	var img = new Image();
+	var steps = 10;
 
-	while (files.length > 0) {
-		
-		var file = files.pop();
-		loadMasterBlairs(file);
-	}
+	img.addEventListener('load', function() {
+
+		var data, pixels;
+
+		for (var i = 0; i < steps; i++) {
+			
+			ctx.drawImage(this, this.width * i, 0);
+
+			data = ctx.getImageData(0, 0, this.width, this.height);
+			pixels = data.data;
+
+			for (var j = 0; j < pixels.length; j += 4) {
+
+				pixels[j] = 255 - (i*255/steps);
+//				pixels[j + 1] = 0;
+//				pixels[j + 2] = 0;
+			}
+
+			ctx.putImageData(data, this.width * i, 0);
+		}
+	});
+
+	img.src = '../static/img/blair.png';
 });
 
-function loadMasterBlairs(file) {
-
-    d3.xml('../static/svg/' + file + '.svg', 'image/svg+xml', function(xml) {
-
-    	var source = $('#source');
-		source.get(0).appendChild(xml.documentElement);
-
-		masterBlairs.push(file);
-		if (masterBlairs.length == numberOfBlairs) {
-			cloneBlairs();
-		}
-    });
-}
-
-function cloneBlairs() {
-
-	var svgs = $('#source svg');
-
-	for (var i = 0; i < masterBlairs.length; i++) {
-
-		var thisBlair = masterBlairs[i];
-
-		$('body').append('<div id="' + thisBlair + '"></div>');
-		var destination = $('#' + thisBlair);
-
-		svgs.each(function(ei, e) {
-
-			if (ei == i) {
-
-				for (var j = 0; j < totalNumberOfBlairs; j++) {
-					$(e).clone().attr('id', thisBlair + j).appendTo(destination);
-					draw($('#' + thisBlair + j + ' path'), j*360/totalNumberOfBlairs);
-				}
-			}
-		});
-	}
-
-	svgs.remove();
-}
-
-function draw(selector, hue) {
-
-	$(selector).each(function(i, e) {
-
-		var fill = e.style['fill'];
-		var color = d3.hsl(fill);
-		color.s = 1;
-		color.h = hue;
-		e.style['fill'] = color.toString();
-
-		e.style['stroke'] = 'none';
-	});
-}
-
 })()
-
-
-// d3.select(window).on("keydown", function () {
-
-// 	switch (d3.event.keyCode) {
-
-// 		// left
-// 		case 37:
-// 			hue = hue - 30;
-// 			if (hue < 0) {
-// 				hue = 360;
-// 			}
-
-// 			draw();
-// 			break;
-
-// 		// right
-// 		case 39:
-// 			hue = hue + 30;
-// 			if (hue > 360) {
-// 				hue = 0;
-// 			}
-
-// 			draw();
-// 			break;
-// 	}
-// });
-
