286aff2fcf1eac12ae9b1334009edd8de65a55c9
Gabriel Florit <gabriel.florit@velir.com>
2011-12-16 07:53:52 -0800

    drawing svgs in HSL

--- gabrielflor_it/static/js/blair.js
+++ gabrielflor_it/static/js/blair.js
@@ -1,48 +1,106 @@
-var hue = 0;
+(function() {
 
-function draw() {
+var masterBlairs;
+var numberOfBlairs;
+var totalNumberOfBlairs = 8;
 
-	var path = d3.selectAll('path')
-		.style('fill', function(d, i) {
+$(document).ready(function() {
 
-			var current = d3.select(this);
-			var fill = current.style('fill');
-			var hsl = d3.hsl(fill);
+	var files = [
+					'blair', 
+					'blaircontrast50'];
+	
+	masterBlairs = [];
+	numberOfBlairs = files.length;
 
-			hsl.s = 360;
-			hsl.h = hue;
 
-			return hsl;
-
-		})
-		.style('stroke', 'none');
-}
-
-d3.select(window).on("keydown", function () {
-
-	switch (d3.event.keyCode) {
-
-		// left
-		case 37:
-			hue--;
-			if (hue < 0) {
-				hue = 360;
-			}
-
-			draw();
-			break;
-
-		// right
-		case 39:
-			hue++;
-			if (hue > 360) {
-				hue = 0;
-			}
-
-			draw();
-			break;
+	while (files.length > 0) {
+		
+		var file = files.pop();
+		loadMasterBlairs(file);
 	}
 });
 
+function loadMasterBlairs(file) {
 
+    d3.xml('../static/svg/' + file + '.svg', 'image/svg+xml', function(xml) {
+
+    	var source = $('#source');
+		source.get(0).appendChild(xml.documentElement);
+
+		masterBlairs.push(file);
+		if (masterBlairs.length == numberOfBlairs) {
+			cloneBlairs();
+		}
+    });
+}
+
+function cloneBlairs() {
+
+	var svgs = $('#source svg');
+
+	for (var i = 0; i < masterBlairs.length; i++) {
+
+		var thisBlair = masterBlairs[i];
+
+		$('body').append('<div id="' + thisBlair + '"></div>');
+		var destination = $('#' + thisBlair);
+
+		svgs.each(function(ei, e) {
+
+			if (ei == i) {
+
+				for (var j = 0; j < totalNumberOfBlairs; j++) {
+					$(e).clone().attr('id', thisBlair + j).appendTo(destination);
+					draw($('#' + thisBlair + j + ' path'), j*360/totalNumberOfBlairs);
+				}
+			}
+		});
+	}
+
+	svgs.remove();
+}
+
+function draw(selector, hue) {
+
+	$(selector).each(function(i, e) {
+
+		var fill = e.style['fill'];
+		var color = d3.hsl(fill);
+		color.s = 1;
+		color.h = hue;
+		e.style['fill'] = color.toString();
+
+		e.style['stroke'] = 'none';
+	});
+}
+
+})()
+
+
+// d3.select(window).on("keydown", function () {
+
+// 	switch (d3.event.keyCode) {
+
+// 		// left
+// 		case 37:
+// 			hue = hue - 30;
+// 			if (hue < 0) {
+// 				hue = 360;
+// 			}
+
+// 			draw();
+// 			break;
+
+// 		// right
+// 		case 39:
+// 			hue = hue + 30;
+// 			if (hue > 360) {
+// 				hue = 0;
+// 			}
+
+// 			draw();
+// 			break;
+// 	}
+// });
 
