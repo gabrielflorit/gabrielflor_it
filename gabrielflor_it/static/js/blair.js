d54728ca923a7ca2be1d24b75745cde22f57e371
Gabriel Florit <gabriel.florit@velir.com>
2011-12-19 08:31:31 -0800

    added more links to county map

--- gabrielflor_it/static/js/blair.js
+++ gabrielflor_it/static/js/blair.js
@@ -6,24 +6,47 @@ $(document).ready(function() {
 	var ctx = canvas.getContext("2d");
 
 	var img = new Image();
-	var steps = 10;
+	var steps = 8;
 
 	img.addEventListener('load', function() {
 
 		var data, pixels;
+		var col1, col2, col3;
+		var r, g, b;
 
 		for (var i = 0; i < steps; i++) {
 			
 			ctx.drawImage(this, this.width * i, 0);
 
-			data = ctx.getImageData(0, 0, this.width, this.height);
+			data = ctx.getImageData(this.width * i, 0, this.width, this.height);
 			pixels = data.data;
 
 			for (var j = 0; j < pixels.length; j += 4) {
 
-				pixels[j] = 255 - (i*255/steps);
-//				pixels[j + 1] = 0;
-//				pixels[j + 2] = 0;
+				r = pixels[j + 0];
+				g = pixels[j + 1];
+				b = pixels[j + 2];
+
+				col1 = chroma.rgb(255, 0, 0);
+				col2 = chroma.rgb(0, 255, 0);
+				col3 = chroma.interpolate(col1, col2, i/steps);
+
+				// rgb: (255, ?, ?)
+				if (i == 0) {
+					pixels[j + 0] = 255 - (255*i/steps);
+				}
+
+				if (i == 1) {
+					pixels[j + 0] = 200;
+					pixels[j + 2] = 55;
+				}
+
+				// rgb: (?, ?, 255)
+				if (i == 7) {
+					pixels[j + 2] = 255;
+				}
+					pixels[j + 0] = 255 - (255*i/steps);
+					pixels[j + 2] = 255 - (255*i/steps);
 			}
 
 			ctx.putImageData(data, this.width * i, 0);
