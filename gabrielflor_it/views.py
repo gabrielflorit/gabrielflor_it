c1188e0a00347c68b9408251c43872d64ff69259
Gabriel Florit <gabriel.florit@velir.com>
2012-01-01 19:12:28 -0500

    better url to new map

--- gabrielflor_it/views.py
+++ gabrielflor_it/views.py
@@ -13,7 +13,7 @@ def counties():
         version=versioning()
         ))
 
-@app.route('/poverty-by-county-shh')
+@app.route('/a-half-decade-of-rising-poverty')
 def poverty_by_county():
     return render_template('d3/poverty-by-county.html', vars=dict(
         version=versioning()
