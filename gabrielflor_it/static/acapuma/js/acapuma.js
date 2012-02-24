0ab68894514f5c76a77e251fab4daf5e3384e851
Gabriel Florit <gabriel.florit@velir.com>
2012-02-24 15:52:09 -0500

    added franchise font to tooltip

--- gabrielflor_it/static/acapuma/js/acapuma.js
+++ gabrielflor_it/static/acapuma/js/acapuma.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function ($) {
 
+	var acapuma = {};
+
 	var url = 'http://api.tiles.mapbox.com/v3/mapbox.world-blank-bright,velir.aca_puma,mapbox.world-bank-borders-en.jsonp';
 	var defaultLat = 39;
 	var defaultLon = -98;
@@ -18,6 +20,65 @@ jQuery(document).ready(function ($) {
 			]
 		);
 		m.setCenterZoom(new mm.Location(defaultLat, defaultLon), defaultZoom);
+		wax.mm.hash(m, tilejson, {
+			defaultCenter: new mm.Location(defaultLat, defaultLon),
+			defaultZoom: defaultZoom,
+			manager: wax.mm.locationHash
+		});
+		interaction = wax.mm.interaction(m, tilejson, 
+			{
+				callbacks: {
+					over: function(feature) {
+						// acapuma.state = feature.split(',')[0];
+						// acapuma.puma = feature.split(',')[1];
+						// acapuma.data = feature.split(',')[2];
+						// drawPopup(feature);
+					},
+					out: function() {
+						// clearPopup();
+					},
+					click: function(feature) {
+					}
+				}
+			}
+		);
+
 	});
+
+// function clearTooltip() {
+// 	$('.wax-tooltip', $('#map')).remove();
+// }
+
+// function drawTooltip(feature) {
+
+// 	clearPopup();
+
+// 	var tooltip = $('<div class="tooltip"/>');
+
+// 	// // find this county
+// 	// var county = companiesPerYear.filter(function(value, index, array) {
+// 	// return value.fips == currentFIPS;
+// 	// })[0];
+
+// 	// var thisDecade = $('.decades .selected').attr('rel');
+
+// 	// var innerHTML = '';
+// 	// innerHTML += "<div class='title'>" + county.name + ' ' + county.type + ', ' + county.state + "</div>";
+// 	// innerHTML += "<div class='numberAndLegend" + (county.companiesPerYear.length > 0 ? '' : ' noData' ) + "'>";
+// 	// innerHTML += "  <div class='number'>" + d3.format('.1f')(Number(county[thisDecade])) + "</div>";
+// 	// innerHTML += "  <div class='legend'>companies per 100,000 people (in " + thisDecade + ")</div>";
+// 	// innerHTML += "</div>";
+// 	// if (county.companiesPerYear.length == 0) {
+// 	// innerHTML += "<div class='noCompanies'>no companies</div>";
+// 	// }
+// 	// innerHTML += "<div id='chart'" + (county.companiesPerYear.length > 0 ? '' : "class='noData'" ) + ">";
+// 	// innerHTML += "</div>";
+
+// 	tooltip.innerHTML = feature;
+// 	$('#map')[0].appendChild(tooltip);
+
+// 	// addChart(county);
+// }
+
 
 });
