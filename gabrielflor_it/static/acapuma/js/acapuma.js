d81184bc9bbcd874f763ac47ad14d2c5f9ce78e3
Gabriel Florit <gabriel.florit@velir.com>
2012-02-24 18:39:47 -0500

    added tooltip

--- gabrielflor_it/static/acapuma/js/acapuma.js
+++ gabrielflor_it/static/acapuma/js/acapuma.js
@@ -2,12 +2,19 @@ jQuery(document).ready(function ($) {
 
 	var acapuma = {};
 
-	var url = 'http://api.tiles.mapbox.com/v3/mapbox.world-blank-bright,velir.aca_puma,mapbox.world-bank-borders-en.jsonp';
+	// var url = 'http://api.tiles.mapbox.com/v3/mapbox.world-black,velir.acapuma.jsonp';
+//mapbox.world-borders-dark
+//	var url = 'http://api.tiles.mapbox.com/v3/mapbox.world-blank-light,velir.acapuma,mapbox.world-bank-borders-en.jsonp';
+	var url = 'http://api.tiles.mapbox.com/v3/mapbox.world-blank-light,velir.acapuma,mapbox.world-borders-dark.jsonp';
 	var defaultLat = 39;
 	var defaultLon = -98;
 	var defaultZoom = 4;
 	var minZoom = 4;
-	var maxZoom = 9;
+	var maxZoom = 11;
+
+	var chart;
+	var iMax = 20;
+	var jMax = 5;
 
 	wax.tilejson(url, function (tilejson) {
 
@@ -29,13 +36,33 @@ jQuery(document).ready(function ($) {
 			{
 				callbacks: {
 					over: function(feature) {
-						// acapuma.state = feature.split(',')[0];
-						// acapuma.puma = feature.split(',')[1];
-						// acapuma.data = feature.split(',')[2];
-						// drawPopup(feature);
+
+						acapuma.state = feature.split(',')[0];
+						acapuma.puma = feature.split(',')[1];
+						acapuma.data = Number(feature.split(',')[2]);
+
+						var bigNumber = acapuma.data;
+						var yesSquares = [];
+						var noSquares = [];
+
+						var scatterData = [];
+						for (var i = 0; i < iMax; i++) {
+							for (var j = 0; j < jMax; j++) {
+								if (bigNumber > 0) {
+									yesSquares.push([i,j]);
+								} else {
+									noSquares.push([i,j]);
+								}
+								bigNumber--;
+							}
+						}
+
+						$('#tooltip .numberAndChart .number').text(acapuma.data + '%');
+						drawChart(yesSquares, noSquares);
+						$('#tooltip').show();
 					},
 					out: function() {
-						// clearPopup();
+						$('#tooltip').hide();
 					},
 					click: function(feature) {
 					}
@@ -45,40 +72,87 @@ jQuery(document).ready(function ($) {
 
 	});
 
-// function clearTooltip() {
-// 	$('.wax-tooltip', $('#map')).remove();
-// }
+	// create basic chart options
+	var options = {
+		chart: {
+			animation: false,
+			renderTo: 'chart',
+			type: 'scatter',
+			height: 90,
+			borderWidth: 0,
+			margin: [0,0,0,0],
+			backgroundColor: '#183966'
+		},
+		credits: {
+			enabled: false
+		},
+		legend: {
+			enabled: false	
+		},
+		yAxis: {
+			gridLineWidth: 0,
+			tickInterval: 1,
+			min: -1,
+			max: jMax,
+			title: {
+				text: ''
+			},
+			labels: {
+				enabled: false
+			}
+		},
+		xAxis: {
+			lineWidth: 0,
+			tickWidth: 0,
+			min: -1,
+			max: iMax,
+			labels: {
+				enabled: false
+			}
+		},
+		title: {
+			text: ''
+		},
+		series: []
+	};
 
-// function drawTooltip(feature) {
+	function drawChart(yesSquares, noSquares) {
 
-// 	clearPopup();
+		if (chart) {
 
-// 	var tooltip = $('<div class="tooltip"/>');
+			chart.series[0].setData(yesSquares, false);
+			chart.series[1].setData(noSquares, false);
+			chart.redraw();
 
-// 	// // find this county
-// 	// var county = companiesPerYear.filter(function(value, index, array) {
-// 	// return value.fips == currentFIPS;
-// 	// })[0];
+		} else {
 
-// 	// var thisDecade = $('.decades .selected').attr('rel');
+			// create the two series
+			var yesSeries = { 
+				marker: {
+					symbol: 'square',
+					fillColor: '#fff',
+					radius: 2.5,
+					lineColor: '#fff',
+					lineWidth: 0.25
+				}
+			};
+			yesSeries.data = yesSquares;
 
-// 	// var innerHTML = '';
-// 	// innerHTML += "<div class='title'>" + county.name + ' ' + county.type + ', ' + county.state + "</div>";
-// 	// innerHTML += "<div class='numberAndLegend" + (county.companiesPerYear.length > 0 ? '' : ' noData' ) + "'>";
-// 	// innerHTML += "  <div class='number'>" + d3.format('.1f')(Number(county[thisDecade])) + "</div>";
-// 	// innerHTML += "  <div class='legend'>companies per 100,000 people (in " + thisDecade + ")</div>";
-// 	// innerHTML += "</div>";
-// 	// if (county.companiesPerYear.length == 0) {
-// 	// innerHTML += "<div class='noCompanies'>no companies</div>";
-// 	// }
-// 	// innerHTML += "<div id='chart'" + (county.companiesPerYear.length > 0 ? '' : "class='noData'" ) + ">";
-// 	// innerHTML += "</div>";
+			var noSeries = { 
+				marker: {
+					symbol: 'square',
+					fillColor: '#183966',
+					radius: 2.5,
+					lineColor: '#fff',
+					lineWidth: 0.25
+				}
+			};
+			noSeries.data = noSquares;
 
-// 	tooltip.innerHTML = feature;
-// 	$('#map')[0].appendChild(tooltip);
+			options.series.push(yesSeries);
+			options.series.push(noSeries);
 
-// 	// addChart(county);
-// }
-
-
+			chart = new Highcharts.Chart(options);
+		}
+	}
 });
