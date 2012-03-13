1c14b4a7ac8c32e1dcd699138112b3c5d1fcf214
Gabriel Florit <gabriel.florit@velir.com>
2012-03-13 14:16:54 -0400

    basic code editor is in place

--- gabrielflor_it/static/water/js/water.js
+++ gabrielflor_it/static/water/js/water.js
@@ -1,26 +1,56 @@
-var data = ['one', 'two', 'three', 'four'];
+$('#code').on('click', function(e) {
+	this.contentEditable = true;
+})
 
-var width = 500;
-var height = 500;
+$('#code').on('keyup', function(e) {
 
-var svg = d3.select('#tree')
-	.append('svg')
-	.style('width', width)
-	.style('height', height);
+	$('svg').empty();
 
-var circles = svg.selectAll('circle')
-	.data(data)
-	.enter()
-	.append('circle');
+	try {
+		eval($(this).text());
+	}
+	catch (error) {}
+	finally {};
+})
 
-circles
-	.attr('cx', function (d, i) {
-		return (i * 50) + 10;
-	})
-	.attr('cy', 10)
-	.attr('r', function (d) {
-		return d.length;
-	})
-	.style('fill', 'red');
+// var data = ['one', 'two', 'three', 'four'];
+
+// var svg = d3.select('#display')
+// 	.append('svg')
+// 	.style('width', '100%')
+// 	.style('height', '99%');
+
+// svg.selectAll('circle')
+// 	.data(data)
+//   .enter()
+// 	.append('circle')
+// 	.attr('cx', function (d, i) {
+// 		return (i * 50) + 10;
+// 	})
+// 	.attr('cy', 10)
+// 	.attr('r', function (d) {
+// 		return d.length;
+// 	})
+// 	.style('fill', 'red');
+
+// $('a').on('click', function(e) {
+// 	e.preventDefault();
+
+// 	var circles = svg.selectAll('circle')
+// 		.data(data)
+// 	  .transition()
+// 	  	.duration(1000)
+// 		.attr('cx', function (d, i) {
+// 			return (i * 60) + 10;
+// 		})
+// 		.attr('cy', 10)
+// 		.attr('r', function (d) {
+// 			return d.length;
+// 		});
+// });
+
+
+
+
 
 
